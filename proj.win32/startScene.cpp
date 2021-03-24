1d799bb0b3224e9f1366f768d5b6e49e7e822414
USER-K27IKB3JPD\USER <ksd3976@naver.com>
2021-03-24 21:41:24 +0900

    스타트신 테스트1

---
+++ proj.win32/startScene.cpp
@@ -0,0 +1,98 @@
+#include "startScene.h"
+#include "gameScene.h"
+#include "EndScene.h"
+//1138, 640
+bool startScene::init()
+{
+	Sprite* back = Sprite::create("background/2.png");
+	back->setPosition(Vec2(569, 320));
+	back->setScaleX(1.32);
+	back->setScaleY(2);
+	addChild(back);
+
+	Sprite* cristal = Sprite::create("UI/icon_ingame_rescueCristal.png");
+	cristal->setPosition(Vec2(410, 610));
+	cristal->setZOrder(1);
+	addChild(cristal);
+
+	Sprite* cristal_ground = Sprite::create("UI/gaugebg_heart02_bossBig.png");
+	cristal_ground->setScaleX(0.1);
+	cristal_ground->setPosition(445, 610);
+	addChild(cristal_ground);
+
+	Sprite* coin = Sprite::create("UI/icon_ingame_relayCoin.png");
+	coin->setPosition(Vec2(540, 610));
+	coin->setZOrder(1);
+	addChild(coin);
+
+	Sprite* coin_ground = Sprite::create("UI/gaugebg_heart02_bossBig.png");
+	coin_ground->setScaleX(0.2);
+	coin_ground->setPosition(605, 610);
+	addChild(coin_ground);
+
+	Sprite* cookieground = Sprite::create("startScene/cookieground.png");
+	cookieground->setScaleY(1.2);
+	addChild(cookieground);
+	cookieground->setPosition(Vec2(900, 300));
+
+	Sprite* itemBoard = Sprite::create("startScene/Itemboard.png");
+	itemBoard->setScale(1.2);
+	itemBoard->setPosition(Vec2(200, 300));
+	addChild(itemBoard);
+
+	Sprite* cookie = Sprite::create("bravecookie/img_tutorial_brave06.png");
+	cookie->setScale(0.5);
+	addChild(cookie);
+	cookie->setPosition(Vec2(900, 340));
+
+	Sprite* rankingBoard = Sprite::create("startScene/rankingboard.png");
+	rankingBoard->setScale(1.2);
+	rankingBoard->setPosition(Vec2(600, 300));
+	addChild(rankingBoard);
+
+	Label* best = Label::createWithTTF("9999", "fonts/SangSangFlowerRoad.otf", 120);
+	addChild(best);
+	best->enableOutline(Color4B::BLACK, 1);
+	best->setPosition(Vec2(600, 270));
+
+	int high = 0;
+	high = UserDefault::getInstance()->getIntegerForKey("myKey");
+
+	int score = UserDefault::getInstance()->getIntegerForKey("myKey");
+	std::string str = StringUtils::format("%04d", score);
+	best->setString(str);
+
+	Button* btn = Button::create("startScene/start_btn.png");
+	btn->setPosition(Vec2(900, 85));
+	btn->setScale(1.5);
+	addChild(btn);
+	btn->addTouchEventListener(CC_CALLBACK_2(startScene::btnClick, this));
+
+	Button* btn2 = Button::create("startScene/exit_btn.png");
+	btn2->setPosition(Vec2(80, 40));
+	btn2->setScale(0.8);
+	addChild(btn2);
+	btn2->addTouchEventListener(CC_CALLBACK_2(startScene::btnClick2, this));
+
+    return true;
+}
+
+void startScene::btnClick(Ref* ref, Widget::TouchEventType type)
+{
+	Button* btn = (Button*)ref;
+	if (type == Widget::TouchEventType::ENDED) {
+		gameScene* game = gameScene::create();
+		Director::getInstance()->replaceScene(game);
+	}
+}
+
+void startScene::btnClick2(Ref* ref, Widget::TouchEventType type)
+{
+	Button* btn = (Button*)ref;
+	if (type == Widget::TouchEventType::ENDED) {
+		exit(0);
+		return;
+	}
+}
+
+
---
+++ proj.win32/startScene.h
@@ -0,0 +1,16 @@
+#pragma execution_character_set("UTF-8")
+#pragma once
+#include <cocos2d.h>
+#include <ui/CocosGUI.h>
+USING_NS_CC;
+using namespace ui;
+class startScene:public Scene
+{
+public:
+	CREATE_FUNC(startScene);
+	bool init();
+
+	void btnClick(Ref* ref, Widget::TouchEventType type);
+	void btnClick2(Ref* ref, Widget::TouchEventType type);
+};
+
